d3edb7f54ac0ee569d0b3bd25212bdd0d47e8880
Kilho <catharsistk@gmail.com>
2016-03-12 23:11:43 +0900

    Add validation process between iterations

--- dl/grad_birnn.py
+++ dl/grad_birnn.py
@@ -25,6 +25,7 @@ NUM_EPOCHS = 10
 BATCH_SIZE = 128
 NUM_HIDDENS = 128
 DISPLAY_STEP = 100
+VALIDATION_FREQUENCY = 1000
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         "..", "data")
@@ -34,6 +35,9 @@ TRAIN_DATA_PATH = os.path.join(DATA_DIR,
 TEST_DATA_PATH = os.path.join(DATA_DIR,
                                "integrated_data_%s_I%d_%s_test.dat" %
                                (TAG, INPUT_DIM, LABEL_NAME))
+
+VALIDATION_DATA_RATIO = float(VALID_SET_RATIO) / \
+                        (TRAIN_SET_RATIO + VALID_SET_RATIO)
 
 FEATURE_IDXS = []
 for s in TAG:   # investigate TAG, character by character
@@ -161,21 +165,42 @@ with tf.Session() as s:
   batch_size = BATCH_SIZE
   display_step = DISPLAY_STEP
   num_epochs = NUM_EPOCHS
-  step = 0
 
   train_data, train_profile_ids, train_labels = \
     extract_data(TRAIN_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
                  IMAGE_HEIGHT, IMAGE_WIDTH,
                  NUM_INPUT_CHANNELS)
-  num_instances, image_height, image_width, _ = train_data.shape
+
+  valid_size = int(len(train_labels) * VALIDATION_DATA_RATIO)
+  valid_data = train_data[:valid_size, :, :, :]
+  valid_labels = train_labels[:valid_size]
+
+  train_data = train_data[valid_size:, :, :, :]
+  train_labels = train_labels[valid_size:]
+  train_size = len(train_labels)
+
+  train_data_shape = train_data.shape
+  valid_data_shape = valid_data.shape
   # train_data: 4D array: [num_instances, image_height,
   #                       [num_features*image_width, num_input_channels]
   # Reshape and transpose data to get 28 sequences
-  train_data = train_data.reshape((num_instances, image_height, image_width))
+  train_data = train_data.reshape(
+    (train_data_shape[0], train_data_shape[1], train_data_shape[2])
+  )
+  valid_data = valid_data.reshape(
+    (valid_data_shape[0], valid_data_shape[1], valid_data_shape[2])
+  )
+
   train_xs = np.transpose(train_data, (0, 2, 1))
   # batch_xs: 3D array: [num_instances, image_width, image_height]
   train_ys = train_labels
-  train_size = len(train_ys)
+
+  valid_xs = np.transpose(valid_data, (0, 2, 1))
+  valid_ys = valid_labels
+
+  validation_frequency = min(train_size, VALIDATION_FREQUENCY)
+  best_validation_acc = 0.0
+  best_step = 0
 
   # Keep training until reach max iterations
   for step in xrange(num_epochs * train_size // batch_size):
@@ -184,30 +209,46 @@ with tf.Session() as s:
     batch_xs = train_xs[offset:(offset+batch_size), :, :]
     batch_ys = train_ys[offset:(offset+batch_size)]
 
-    feed_dict = {x: batch_xs, y: batch_ys,
-                 istate_fw: np.zeros((batch_size, 2*n_hidden)),
-                 istate_bw: np.zeros((batch_size, 2*n_hidden))}
+    train_feed_dict = {x: batch_xs, y: batch_ys,
+                       istate_fw: np.zeros((batch_size, 2*n_hidden)),
+                       istate_bw: np.zeros((batch_size, 2*n_hidden))}
     # Fit training using batch data
-    s.run(optimizer, feed_dict=feed_dict)
+    s.run(optimizer, feed_dict=train_feed_dict)
 
     curr_epoch_in_float = float(step) * batch_size / train_size
 
     if step % display_step == 0:
       # Calculate batch accuracy
-      acc = s.run(accuracy, feed_dict=feed_dict)
+      acc = s.run(accuracy, feed_dict=train_feed_dict)
       # Calculate batch loss
-      loss = s.run(cost, feed_dict=feed_dict)
+      loss = s.run(cost, feed_dict=train_feed_dict)
 
       print("@ Epoch %.2f" % curr_epoch_in_float)
       print("Minibatch loss: %.6f" % loss)
       print("Minibatch error: %.5f" % (1.0 - acc))
 
+    if step % validation_frequency == 0:
+      valid_feed_dict = {x: valid_xs, y: valid_ys,
+                         istate_fw: np.zeros((valid_size, 2*n_hidden)),
+                         istate_bw: np.zeros((valid_size, 2*n_hidden))}
+      this_validation_acc = s.run(accuracy, feed_dict=valid_feed_dict)
+      print("   Validation error: %.5f" % (1.0 - this_validation_acc))
+
+      # If we got the best validation score until now
+      if this_validation_acc > best_validation_acc:
+        # Save the best validation score and step number
+        best_validation_acc = this_validation_acc
+        best_step = step
+
+
   duration = time.time() - start_time
-  print "Optimization Finished!"
+  print("Optimization Finished!")
+  print("Best validation error: %.5f obtained at epoch %.2f"
+        % (1.0 - best_validation_acc,
+           float(best_step) * batch_size / train_size))
   print >> sys.stderr, ("The session ran for %.2fm" % (duration / 60.))
 
   # Calculate accuracy for test data
-
   test_data, test_profile_ids, test_labels =\
     extract_data(TEST_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
                  IMAGE_HEIGHT, IMAGE_WIDTH,
@@ -221,12 +262,12 @@ with tf.Session() as s:
   # batch_xs: 3D array: [num_instances, image_width, image_height]
   test_ys = test_labels
   test_len = 1000
-  test_ys = test_ys[:1000, :]
+  test_ys = test_ys[:test_len, :]
 
-  feed_dict = {x: test_xs, y: test_ys,
-               istate_fw: np.zeros((test_len, 2*n_hidden)),
-               istate_bw: np.zeros((test_len, 2*n_hidden))}
-  print "Testing Accuracy: ", s.run(accuracy, feed_dict=feed_dict)
+  test_feed_dict = {x: test_xs, y: test_ys,
+                    istate_fw: np.zeros((test_len, 2*n_hidden)),
+                    istate_bw: np.zeros((test_len, 2*n_hidden))}
+  print "Testing Accuracy: ", s.run(accuracy, feed_dict=test_feed_dict)
 
 
 
