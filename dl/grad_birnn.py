ca6d7796c15104fa1ff11939d1e076766ce75718
Kilho <catharsistk@gmail.com>
2016-03-13 17:12:45 +0900

    Try with AudioFeatures and MagneticFieldSensor data

--- dl/grad_birnn.py
+++ dl/grad_birnn.py
@@ -6,13 +6,15 @@ import numpy as np
 import os
 import sys
 import time
-from util.preprocess_data import FEATURE_IDXS_DICT, FEATURE_NAMES_DICT
+# from util.preprocess_data import FEATURE_IDXS_DICT, FEATURE_NAMES_DICT
+# from util.preprocess_audio_data import FEATURE_IDXS_DICT, FEATURE_NAMES_DICT
+from util.preprocess_magneticfieldsensor_data import FEATURE_IDXS_DICT, FEATURE_NAMES_DICT
 
 
 # FIXME: Parameters
-TAG = "A"
-INPUT_DIM = 64
-LABEL_NAME = "having_a_meal"
+TAG = "M"   # FIXME
+INPUT_DIM = 16  # FIXME
+LABEL_NAME = "accompanying"  # FIXME
 
 IMAGE_HEIGHT = 1
 IMAGE_WIDTH = INPUT_DIM
@@ -20,11 +22,11 @@ NUM_INPUT_CHANNELS = 1
 NUM_CLASSES = 2
 
 LEARNING_RATE = 0.001
-NUM_EPOCHS = 10
+NUM_EPOCHS = 20
 BATCH_SIZE = 128
 NUM_HIDDENS = 128
-DISPLAY_STEP = 100
-VALIDATION_FREQUENCY = 1000
+DISPLAY_STEP = 50
+VALIDATION_FREQUENCY = 150
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         "..", "data")
@@ -35,13 +37,13 @@ TEST_DATA_PATH = os.path.join(DATA_DIR,
                                "integrated_data_%s_I%d_%s_test.dat" %
                                (TAG, INPUT_DIM, LABEL_NAME))
 
-VALIDATION_SIZE = 128
-TEST_SIZE = 128   # FIXME
-# TRAIN_SET_RATIO = 7
-# VALID_SET_RATIO = 1
-#
-# VALIDATION_DATA_RATIO = float(VALID_SET_RATIO) / \
-#                         (TRAIN_SET_RATIO + VALID_SET_RATIO)
+# VALIDATION_SIZE = 128
+# TEST_SIZE = 128   # FIXME
+TRAIN_SET_RATIO = 7
+VALID_SET_RATIO = 1
+
+VALIDATION_DATA_RATIO = float(VALID_SET_RATIO) / \
+                        (TRAIN_SET_RATIO + VALID_SET_RATIO)
 
 FEATURE_IDXS = []
 for s in TAG:   # investigate TAG, character by character
@@ -55,6 +57,7 @@ for i in range(len(FEATURE_IDXS)):
     NUM_FEATURES += FEATURE_IDXS[i]
 
 # Network parameters
+batch_size = BATCH_SIZE
 n_input = IMAGE_HEIGHT	  # Sensor data input (shape: 1*64)
 n_steps = INPUT_DIM*NUM_FEATURES  # timesteps
 n_hidden = NUM_HIDDENS  # number of features in hidden layers
@@ -98,13 +101,49 @@ def extract_data(filename, num_features, num_classes,
 
     return data, profile_ids, labels
 
+train_data, train_profile_ids, train_labels =\
+  extract_data(TRAIN_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
+               IMAGE_HEIGHT, IMAGE_WIDTH,
+               NUM_INPUT_CHANNELS)
+
+valid_size = int(len(train_labels) * VALIDATION_DATA_RATIO)
+# valid_size = VALIDATION_SIZE
+valid_data = train_data[:valid_size, :, :, :]
+valid_labels = train_labels[:valid_size]
+
+train_data = train_data[valid_size:, :, :, :]
+train_labels = train_labels[valid_size:]
+train_size = len(train_labels)
+
+train_data_shape = train_data.shape
+valid_data_shape = valid_data.shape
+# train_data: 4D array: [num_instances, image_height,
+#                       [num_features*image_width, num_input_channels]
+# Reshape and transpose data to get 28 sequences
+train_data = train_data.reshape(
+  (train_data_shape[0], train_data_shape[1], train_data_shape[2])
+)
+valid_data = valid_data.reshape(
+  (valid_data_shape[0], valid_data_shape[1], valid_data_shape[2])
+)
+
+
+train_data = np.transpose(train_data, (0, 2, 1))
+# batch_xs: 3D array: [num_instances, image_width, image_height]
+
+valid_data = np.transpose(valid_data, (0, 2, 1))
 
 # tf Graph input
-x = tf.placeholder("float", [None, n_steps, n_input])
+train_x = tf.placeholder(tf.float32, shape=(batch_size, n_steps, n_input))
 # TensorFlow LSTM cell requires 2x n_hidden length (state & cell)
-istate_fw = tf.placeholder("float", [None, 2*n_hidden])
-istate_bw = tf.placeholder("float", [None, 2*n_hidden])
-y = tf.placeholder("float", [None, n_classes])
+train_istate_fw = tf.placeholder(tf.float32, shape=(batch_size, 2*n_hidden))
+train_istate_bw = tf.placeholder(tf.float32, shape=(batch_size, 2*n_hidden))
+train_y = tf.placeholder(tf.float32, shape=(batch_size, n_classes))
+
+valid_x = tf.constant(valid_data)
+valid_istate_fw = tf.zeros([valid_size, 2*n_hidden], dtype=tf.float32)
+valid_istate_bw = tf.zeros([valid_size, 2*n_hidden], dtype=tf.float32)
+valid_y = tf.constant(valid_labels)
 
 # Define weights
 weights = {
@@ -149,15 +188,25 @@ def BiRNN(_X, _istate_fw, _istate_bw, _weights, _biases, _batch_size, _seq_len):
   # Get inner loop last output
   return tf.matmul(outputs[-1], _weights['out']) + _biases['out']
 
-pred = BiRNN(x, istate_fw, istate_bw, weights, biases, BATCH_SIZE, n_steps)
+with tf.variable_scope("model", reuse=None):
+  train_pred = BiRNN(train_x, train_istate_fw, train_istate_bw,
+                     weights, biases, BATCH_SIZE, n_steps)
 
 # Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y)) # Softmax loss
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(train_pred, train_y)) # Softmax loss
 optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cost) # Adam Optimizer
 
 # Evaluate model
-correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+train_correct_pred = tf.equal(tf.argmax(train_pred, 1), tf.argmax(train_y, 1))
+train_accuracy = tf.reduce_mean(tf.cast(train_correct_pred, tf.float32))
+
+# Validation
+with tf.variable_scope("model", reuse=True):
+  valid_pred = BiRNN(valid_x, valid_istate_fw, valid_istate_bw,
+                     weights, biases, valid_size, n_steps)
+valid_pred_1 = tf.reduce_sum(tf.argmax(valid_pred, 1))
+valid_correct_pred = tf.equal(tf.argmax(valid_pred, 1), tf.argmax(valid_y, 1))
+valid_accuracy = tf.reduce_mean(tf.cast(valid_correct_pred, tf.float32))
 
 # Initializing the variables
 init = tf.initialize_all_variables()
@@ -166,42 +215,8 @@ init = tf.initialize_all_variables()
 with tf.Session() as s:
   start_time = time.time()
   s.run(init)
-  batch_size = BATCH_SIZE
   display_step = DISPLAY_STEP
   num_epochs = NUM_EPOCHS
-
-  train_data, train_profile_ids, train_labels = \
-    extract_data(TRAIN_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
-                 IMAGE_HEIGHT, IMAGE_WIDTH,
-                 NUM_INPUT_CHANNELS)
-
-  # valid_size = int(len(train_labels) * VALIDATION_DATA_RATIO)
-  valid_size = VALIDATION_SIZE
-  valid_data = train_data[:valid_size, :, :, :]
-  valid_labels = train_labels[:valid_size]
-
-  train_data = train_data[valid_size:, :, :, :]
-  train_labels = train_labels[valid_size:]
-  train_size = len(train_labels)
-
-  train_data_shape = train_data.shape
-  valid_data_shape = valid_data.shape
-  # train_data: 4D array: [num_instances, image_height,
-  #                       [num_features*image_width, num_input_channels]
-  # Reshape and transpose data to get 28 sequences
-  train_data = train_data.reshape(
-    (train_data_shape[0], train_data_shape[1], train_data_shape[2])
-  )
-  valid_data = valid_data.reshape(
-    (valid_data_shape[0], valid_data_shape[1], valid_data_shape[2])
-  )
-
-  train_xs = np.transpose(train_data, (0, 2, 1))
-  # batch_xs: 3D array: [num_instances, image_width, image_height]
-  train_ys = train_labels
-
-  valid_xs = np.transpose(valid_data, (0, 2, 1))
-  valid_ys = valid_labels
 
   validation_frequency = min(train_size, VALIDATION_FREQUENCY)
   best_validation_acc = 0.0
@@ -211,12 +226,12 @@ with tf.Session() as s:
   for step in xrange(num_epochs * train_size // batch_size):
     # Compute the offset of the current minibatch in the data.
     offset = (step * batch_size) % (train_size - batch_size)
-    batch_xs = train_xs[offset:(offset+batch_size), :, :]
-    batch_ys = train_ys[offset:(offset+batch_size)]
+    batch_data = train_data[offset:(offset+batch_size), :, :]
+    batch_labels = train_labels[offset:(offset+batch_size)]
 
-    train_feed_dict = {x: batch_xs, y: batch_ys,
-                       istate_fw: np.zeros((batch_size, 2*n_hidden)),
-                       istate_bw: np.zeros((batch_size, 2*n_hidden))}
+    train_feed_dict = {train_x: batch_data, train_y: batch_labels,
+                       train_istate_fw: np.zeros((batch_size, 2*n_hidden)),
+                       train_istate_bw: np.zeros((batch_size, 2*n_hidden))}
     # Fit training using batch data
     s.run(optimizer, feed_dict=train_feed_dict)
 
@@ -224,7 +239,7 @@ with tf.Session() as s:
 
     if step % display_step == 0:
       # Calculate batch accuracy
-      acc = s.run(accuracy, feed_dict=train_feed_dict)
+      acc = s.run(train_accuracy, feed_dict=train_feed_dict)
       # Calculate batch loss
       loss = s.run(cost, feed_dict=train_feed_dict)
 
@@ -233,10 +248,9 @@ with tf.Session() as s:
       print("Minibatch error: %.5f" % (1.0 - acc))
 
     if step % validation_frequency == 0:
-      valid_feed_dict = {x: valid_xs, y: valid_ys,
-                         istate_fw: np.zeros((valid_size, 2*n_hidden)),
-                         istate_bw: np.zeros((valid_size, 2*n_hidden))}
-      this_validation_acc = s.run(accuracy, feed_dict=valid_feed_dict)
+      valid_p_1, this_validation_acc = \
+        s.run([valid_pred_1, valid_accuracy])
+      print("   # Accompayning: %d / %d" % (valid_p_1, valid_size))
       print("   Validation error: %.5f" % (1.0 - this_validation_acc))
 
       # If we got the best validation score until now
@@ -245,7 +259,6 @@ with tf.Session() as s:
         best_validation_acc = this_validation_acc
         best_step = step
 
-
   duration = time.time() - start_time
   print("Optimization Finished!")
   print("Best validation error: %.5f obtained at epoch %.2f"
@@ -253,29 +266,29 @@ with tf.Session() as s:
            float(best_step) * batch_size / train_size))
   print >> sys.stderr, ("The session ran for %.2fm" % (duration / 60.))
 
-  # Calculate accuracy for test data
-  test_data, test_profile_ids, test_labels =\
-    extract_data(TEST_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
-                 IMAGE_HEIGHT, IMAGE_WIDTH,
-                 NUM_INPUT_CHANNELS)
-  num_instances, image_height, image_width, _ = test_data.shape
-  # test_data: 4D array: [num_instances, image_height,
-  #                       [num_features*image_width, num_input_channels]
-  # Reshape and transpose data to get 28 sequences
-  test_data = test_data.reshape((num_instances, image_height, image_width))
-  test_xs = np.transpose(test_data, (0, 2, 1))
-  # batch_xs: 3D array: [num_instances, image_width, image_height]
-  test_ys = test_labels
-  test_size = TEST_SIZE
-  test_ys = test_ys[:test_size, :]
-
-  test_feed_dict = {x: test_xs, y: test_ys,
-                    istate_fw: np.zeros((test_size, 2*n_hidden)),
-                    istate_bw: np.zeros((test_size, 2*n_hidden))}
-  print "Testing Accuracy: ", s.run(accuracy, feed_dict=test_feed_dict)
-
-
-
+  # # Calculate accuracy for test data
+  # test_data, test_profile_ids, test_labels =\
+  #   extract_data(TEST_DATA_PATH, NUM_FEATURES, NUM_CLASSES,
+  #                IMAGE_HEIGHT, IMAGE_WIDTH,
+  #                NUM_INPUT_CHANNELS)
+  # num_instances, image_height, image_width, _ = test_data.shape
+  # # test_data: 4D array: [num_instances, image_height,
+  # #                       [num_features*image_width, num_input_channels]
+  # # Reshape and transpose data to get 28 sequences
+  # test_data = test_data.reshape((num_instances, image_height, image_width))
+  # test_xs = np.transpose(test_data, (0, 2, 1))
+  # # batch_xs: 3D array: [num_instances, image_width, image_height]
+  # test_ys = test_labels
+  # test_size = TEST_SIZE
+  # test_ys = test_ys[:test_size, :]
+  #
+  # test_feed_dict = {x: test_xs, y: test_ys,
+  #                   istate_fw: np.zeros((test_size, 2*n_hidden)),
+  #                   istate_bw: np.zeros((test_size, 2*n_hidden))}
+  # print "Testing Accuracy: ", s.run(accuracy, feed_dict=test_feed_dict)
+  #
+  #
+  #
 
 
 
